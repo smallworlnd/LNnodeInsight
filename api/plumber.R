641be72f64f601ea9f9245c4d72168005b23154f
smallworlnd <smallworlnd@protonmail.com>
2021-07-30 14:54:29 -0400

    modified api variable names to be more user-friendly

--- api/plumber.R
+++ api/plumber.R
@@ -2,5 +2,5 @@
 #* @get /centralityranks
 
 function() {
-	g %>% select(name, cent.between.rank, cent.close.rank, cent.eigen.rank) %>% as_tibble %>% rename('pubkey'='name')
+	g %>% select(name, cent.between.rank, cent.close.rank, cent.eigen.rank) %>% as_tibble %>% rename('pubkey'='name', 'cent.between.rank'='cent_between_rank', 'cent.close.rank'='cent_close_rank', 'cent.eigen.rank'='cent_eigen_rank')
 }
